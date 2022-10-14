ee9ffcfe34a0ab86f36a0ba661ea37e7ef81d7e1
Adam Iverson <aciverso@gmail.com>
2022-10-14 16:09:29 -0500

    observe DOM changes

--- analytics-test/src/App.js
+++ analytics-test/src/App.js
@@ -9,7 +9,7 @@ import TagManager from 'react-gtm-module';
 function App() {
 
   useEffect(() => {
-    TagManager.initialize({ gtmId: 'GTM-NR4FJGF' })
+    TagManager.initialize({ gtmId: 'GTM-M76J5TP' })
   }, []);
 
   return (
